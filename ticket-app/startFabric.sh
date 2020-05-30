362940fe785134e08714a5fe886e6c72a90e277e
Nhatcao <mitomdasem@gmail.com>
2020-05-30 10:52:24 +0700

    a

--- ticket-app/startFabric.sh
+++ ticket-app/startFabric.sh
@@ -25,12 +25,12 @@ printf "\nCalled start.sh\n\n"
 # and prime the ledger with our 10 tuna catches
 docker-compose -f ./docker-compose.yml up -d cli
 printf "\ncli started\n\n"
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n ticket-app -v 1.0 -p github.com/ticket-app
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n ticket-app -v 1.0 -p https://github.com/nhatcaoo/Ticketing/chaincode/ticket-app/
 printf "\nChaincode installed\n\n"
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n ticket-app -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 printf "\nchaincode instantiated\n\n"
 sleep 10
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n ticket-app -c '{"function":"initEvent","Args":[""]}'
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n ticket-app -c 
 printf "\nchaincode invoked\n\n"
 example.com:7050 -C mychannel 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
