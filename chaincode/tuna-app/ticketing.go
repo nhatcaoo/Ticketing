586ebe429730ded235ed91d8f5b21a79c7774511
Nhatcao <mitomdasem@gmail.com>
2020-06-02 17:09:44 +0700

    a

--- chaincode/tuna-app/ticketing.go
+++ chaincode/tuna-app/ticketing.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"strconv"
 	"time"
 
@@ -65,32 +66,33 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 	return shim.Error("Wrong function name.")
 }
 func (s *SmartContract) initEvent(APIstub shim.ChaincodeStubInterface) sc.Response {
-	// log.Fatalf("start: ")
-	// events := []Event{
-	// 	Event{ID: 0, Issuer: "VFF", Price: "220.000", EventName: "Suzuki cup", Total: 20, Sold: 0},
-	// 	Event{ID: 1, Issuer: "BFF", Price: "220.000", EventName: "B cup", Total: 20, Sold: 0},
-	// 	Event{ID: 2, Issuer: "CFF", Price: "220.000", EventName: "C cup", Total: 20, Sold: 0},
-	// 	Event{ID: 3, Issuer: "DFF", Price: "220.000", EventName: "D cup", Total: 20, Sold: 0},
-	// 	Event{ID: 4, Issuer: "EFF", Price: "220.000", EventName: "F cup", Total: 20, Sold: 0}}
-	// log.Fatalf("done 1: ")
-	// j := 0
-	// for j < 5 {
-	// 	eventAsBytes, _ := json.Marshal(events[j])
-	// 	APIstub.PutState("EVENT"+strconv.Itoa(events[j].ID), eventAsBytes)
-	// 	for i := 0; i < events[j].Total; i++ {
+	log.Fatalf("start: ")
+	logger.Info("get_caller_data called")
+	events := []Event{
+		Event{ID: 0, Issuer: "VFF", Price: "220.000", EventName: "Suzuki cup", Total: 20, Sold: 0},
+		Event{ID: 1, Issuer: "BFF", Price: "220.000", EventName: "B cup", Total: 20, Sold: 0},
+		Event{ID: 2, Issuer: "CFF", Price: "220.000", EventName: "C cup", Total: 20, Sold: 0},
+		Event{ID: 3, Issuer: "DFF", Price: "220.000", EventName: "D cup", Total: 20, Sold: 0},
+		Event{ID: 4, Issuer: "EFF", Price: "220.000", EventName: "F cup", Total: 20, Sold: 0}}
+	log.Fatalf("done 1: ")
+	j := 0
+	for j < 5 {
+		eventAsBytes, _ := json.Marshal(events[j])
+		APIstub.PutState("EVENT"+strconv.Itoa(events[j].ID), eventAsBytes)
+		for i := 0; i < events[j].Total; i++ {
 
-	// 		var ticket = Ticket{EventId: events[i].ID, TicketId: strconv.Itoa(events[i].ID) + "-" + strconv.Itoa(i), Cost: events[i].Price, CurrentOwner: "N/A", OnSell: true, TimeStamp: time.Now(), IsRedeemed: false}
-	// 		ticketAsBytes, _ := json.Marshal(ticket)
-	// 		APIstub.PutState("TICKET"+ticket.TicketId, ticketAsBytes)
-	// 		log.Fatalf("-\n ")
-	// 	}
-	// 	j = j + 1
-	// }
-	// fmt.Printf("done 2: ")
-	// var info = Info{}
-	// info.number = 5
-	// infoAsBytes, _ := json.Marshal(info)
-	// APIstub.PutState("NUMBER_EVENTS", infoAsBytes)
+			var ticket = Ticket{EventId: events[i].ID, TicketId: strconv.Itoa(events[i].ID) + "-" + strconv.Itoa(i), Cost: events[i].Price, CurrentOwner: "N/A", OnSell: true, TimeStamp: time.Now(), IsRedeemed: false}
+			ticketAsBytes, _ := json.Marshal(ticket)
+			APIstub.PutState("TICKET"+ticket.TicketId, ticketAsBytes)
+			log.Fatalf("-\n ")
+		}
+		j = j + 1
+	}
+	fmt.Printf("done 2: ")
+	var info = Info{}
+	info.number = 5
+	infoAsBytes, _ := json.Marshal(info)
+	APIstub.PutState("NUMBER_EVENTS", infoAsBytes)
 	return shim.Success(nil)
 }
 func (s *SmartContract) buyTicketFromSupplier(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
