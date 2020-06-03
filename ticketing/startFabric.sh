157b5ebddba15fa1ebc42d23ec682428524af86e
Nhatcao <mitomdasem@gmail.com>
2020-06-03 10:16:00 +0700

    a

--- ticketing/startFabric.sh
+++ ticketing/startFabric.sh
@@ -16,19 +16,22 @@ starttime=$(date +%s)
 if [ ! -d ~/.hfc-key-store/ ]; then
 	mkdir ~/.hfc-key-store/
 fi
-
+printf "\nKey store created\n\n"
 # launch network; create channel and join peer to channel
 cd ../basic-network
 ./start.sh
-
+printf "\nCalled start.sh\n\n"
 # Now launch the CLI container in order to install, instantiate chaincode
 # and prime the ledger with our 10 tuna catches
 docker-compose -f ./docker-compose.yml up -d cli
-
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+printf "\ncli started\n\n"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n ticketing -v 2.0 -p github.com/ticketing
+printf "\nChaincode installed\n\n"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n ticketing -v 2.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+printf "\nchaincode instantiated\n\n"
 sleep 10
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n tuna-app -c '{"function":"initEvent","Args":[""]}'
-
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n ticketing -c '{"function":"initEvent","Args":[""]}'
+printf "\nchaincode invoked\n\n"
+example.com:7050 -C mychannel 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
-printf "\nStart with the registerAdmin.js, then registerUser.js, then server.js\n\n"
+printf "\nStart with the registerAdmin.js, then registerUser.js, then server.js\n\n" 
